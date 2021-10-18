627b08fec1348f8cbeccc3bd61d721d2e84c8ddd
Zain-de94 <71317396+Zain-de94@users.noreply.github.com>
2021-10-18 15:35:44 +0530

    Android trivia Step-1 Solution Adding and Creating Fragment

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,19 +1,26 @@
 package com.example.android.navigation
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
-import com.example.android.navigation.databinding.FragmentGameOverBinding
+import com.example.android.navigation.databinding.FragmentTitleBinding
 
-class GameOverFragment : Fragment() {
+
+class TitleFragment : Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_game_over, container, false)
+
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(
+            inflater , R.layout.fragment_title,container ,false);
+
         return binding.root
     }
+
+
+
+
 }
