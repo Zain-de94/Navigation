1f26b0168263f9c6c4652e416c771b4918b9dd70
Zain-de94 <71317396+Zain-de94@users.noreply.github.com>
2021-10-22 14:28:37 +0530

    Android-trivia-Step-4-Backstack Manipulation

---
+++ app/src/main/java/com/example/navigation/GameWonFragment.kt
@@ -0,0 +1,29 @@
+package com.example.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.navigation.databinding.FragmentGameWonBinding
+
+
+class GameWonFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_game_won, container, false)
+
+        binding.nextMatchButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
+        }
+
+        return binding.root
+    }
+
+
+}
