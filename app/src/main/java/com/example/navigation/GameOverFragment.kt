cbfce20cb2e45e82ac81e7a93821fa73687628bb
Zain-de94 <71317396+Zain-de94@users.noreply.github.com>
2021-10-23 15:11:46 +0530

    Android-trivia-Step-5-Adding-The-About_Fragment

---
+++ app/src/main/java/com/example/navigation/GameOverFragment.kt
@@ -0,0 +1,28 @@
+package com.example.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.navigation.databinding.FragmentGameOverBinding
+
+
+class GameOverFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_game_over, container, false)
+
+        binding.tryAgainButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_gameOverFragment2_to_gameFragment)
+        }
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/navigation/TitleFragment.kt
@@ -0,0 +1,37 @@
+package com.example.navigation
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.navigation.databinding.FragmentTitleBinding
+
+
+class TitleFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(
+            inflater , R.layout.fragment_title,container ,false);
+
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,
+        requireView().findNavController())
+                ||super.onOptionsItemSelected(item)
+    }
+}
