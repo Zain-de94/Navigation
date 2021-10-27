6681bcc5f2a1296844b1fbba860e48a2b7ef9741
Zain-de94 <71317396+Zain-de94@users.noreply.github.com>
2021-10-27 23:19:29 +0530

    Android-trivia-Step-9-Solution-Adding-the-Navigation-Drawer

---
+++ app/src/main/java/com/example/navigation/GameWonFragment.kt
@@ -0,0 +1,65 @@
+package com.example.navigation
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import android.widget.Toast
+import androidx.core.app.ShareCompat
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.navigation.databinding.FragmentGameWonBinding
+
+
+class GameWonFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_game_won, container, false)
+
+        binding.nextMatchButton.setOnClickListener { view: View ->
+            view.findNavController().
+            navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
+        }
+
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    private fun getShareIntent(): Intent{
+
+        var args = GameWonFragmentArgs.fromBundle(requireArguments())
+
+        return ShareCompat.IntentBuilder.from(requireActivity())
+            .setText(getString(R.string.share_success_text,args.numCorrect
+            ,args.numQuestions))
+            .setType("text/plain")
+            .intent
+
+    }
+
+    private fun shareSuccess(){
+        startActivity(getShareIntent())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.winner_menu,menu)
+
+        if(null==getShareIntent().resolveActivity(requireActivity().packageManager)){
+
+            menu.findItem(R.id.share)?.isVisible = false
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
---
+++ app/src/main/java/com/example/navigation/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.example.navigation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(
+            this,
+            R.layout.activity_main
+        )
+        drawerLayout = binding.drawerLayout
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this,
+            navController,drawerLayout)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph , drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView , navController)
+
+
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController,appBarConfiguration)
+    }
+}
